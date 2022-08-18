f6426fa8e1efa33c309f73e740d1ff2e55029c9c
Clemence Parodi <clemparodi1609@hotmail.com>
2022-08-18 17:00:16 +0200

    progress day 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  
+  delete "/bookmarks/:id", to: "bookmarks#destroy", as: :delete_bookmark
+  # resources :bookmarks, only: [:destroy] as del_bookmark
 end
